69a5da9aaca6fd8240fd88fbe5b7deec8340a727
DimaZimin <60842294+DimaZimin@users.noreply.github.com>
2020-06-26 00:25:35 +0200

    Update math_problems.py

--- math_problems.py
+++ math_problems.py
@@ -14,3 +14,20 @@ def palindrome():
                     continue
     return n
    
+
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+def gcd(x,y): return y and gcd(y, x % y) or x
+def lcm(x,y): return x * y / gcd(x,y)
+
+n = 1
+for i in range(1, 21):
+     n = lcm(n, i)
+        
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.   
+def sumofsq():
+    return sum([x*x for x in range(1, 101)])
+
+def squareofthesum():
+    return sum([x for x in range(1, 101)])**2
+print(squareofthesum() - sumofsq())
+#25164150
